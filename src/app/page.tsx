8e8162ae0826a604dc256863aee4b254cab95ed8
OpsecFreak <143857880+opsecfreak@users.noreply.github.com>
2025-09-04 19:37:09 -0400

    animations and cleanups.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ import ProtectedRoute from "../components/auth/ProtectedRoute";
 export default function Home() {
   return (
     <ProtectedRoute>
-      <div className="flex items-center justify-center min-h-screen p-4 sm:p-6">
-        <div className="w-full max-w-4xl h-[calc(100vh-2rem)]">
+      <div className="flex items-center justify-center min-h-screen p-2 sm:p-4">
+        <div className="w-full max-w-5xl h-[calc(100vh-1rem)]">
           <ChatBox />
         </div>
       </div>
